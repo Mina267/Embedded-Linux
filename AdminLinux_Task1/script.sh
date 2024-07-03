492f9e63174f6e920a6cc98255a7db67e03bc3d2
Mina Magdy Aziz <71601701+Mina267@users.noreply.github.com>
2024-07-03 22:06:12 +0300

    Add Last Exercice script to bash script file to AdminLinux_Task1

--- AdminLinux_Task1/script.sh
+++ AdminLinux_Task1/script.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+
+# 1. Create a directory called "myDirectory" in your home folder.
+mkdir ~/myDirectory
+
+# 2. Inside "myDirectory," create another directory named "secondDirectory."
+mkdir ~/myDirectory/secondDirectory
+
+# 3. Within "secondDirectory," create a file named "myNotePaper."
+touch ~/myDirectory/secondDirectory/myNotePaper
+
+# 4. Copy "myNotePaper" from "secondDirectory" to "myDirectory."
+cp ~/myDirectory/secondDirectory/myNotePaper ~/myDirectory/
+
+# 5. Rename the copied file in "myDirectory" to "myOldNotePaper."
+mv ~/myDirectory/myNotePaper ~/myDirectory/myOldNotePaper
